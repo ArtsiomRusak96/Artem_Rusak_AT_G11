2d547e8a49b33aa53af45cae236f40cb59d066cf
Artem Rusak <tema.rusak88@gmail.com>
2024-11-10 18:19:22 +0300

    array with filtered values in reverse order

--- src/homework/day4/ArraysAndConsoleTask.java
+++ src/homework/day4/ArraysAndConsoleTask.java
@@ -8,13 +8,19 @@ public class ArraysAndConsoleTask {
 
         ArraysAndConsoleTask arraysAndConsoleTask = new ArraysAndConsoleTask();
 
-        int[] array = new int[100];
+        int[] array = new int[20];
         int n = 7;
         Random random = new Random();
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(100);
         }
+
         arraysAndConsoleTask.sumArrayValues(array, n);
+
+        int[] newArray = arraysAndConsoleTask.printArrayValuesInReverse(array, n);
+        for (int i : newArray) {
+            System.out.println(i);
+        }
     }
 
     int sum = 0;
@@ -25,5 +31,23 @@ public class ArraysAndConsoleTask {
             sum += array[i];
         }
         System.out.println(sum);
+    }
+
+    public int[] printArrayValuesInReverse(int[] array, int n) {
+        int countElements = 0;
+        for (int i : array) {
+//            System.out.println(i);
+            if (i > n) {
+                countElements++;
+            }
+        }
+        int[] newArray = new int[countElements];
+        int j = 0;
+        for (int i = array.length - 1; i > -1; i--) {
+            if (array[i] > n) {
+                newArray[j++] = array[i];
+            }
+        }
+        return newArray;
     }
 }
