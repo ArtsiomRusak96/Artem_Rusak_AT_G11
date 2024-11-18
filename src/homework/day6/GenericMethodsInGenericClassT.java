c47ffbe893abd560d95bab15cceb9d9329d2325e
Artem Rusak <tema.rusak88@gmail.com>
2024-11-19 01:08:34 +0300

    method with two generic arguments

--- src/homework/day6/GenericMethodsInGenericClassT.java
+++ src/homework/day6/GenericMethodsInGenericClassT.java
@@ -1,8 +1,12 @@
 package homework.day6;
 
 public class GenericMethodsInGenericClassT<T> {
-
+    
     public void genericMethodOneGenArg(T arg) {
         System.out.printf("I am object of %s class", arg.getClass().getSimpleName());
+    }
+
+    public String genericMethodTwoGenArgs(T arg1, T arg2) {
+        return "We are objects of " + arg1.getClass().getSimpleName() + " class and " + arg2.getClass().getSimpleName() + " class";
     }
 }
