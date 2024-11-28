f8e454e2f4c1de0c8107b077adfd23bac16b3bf8
Artem Rusak <tema.rusak88@gmail.com>
2024-11-28 20:38:03 +0300

    read, replace and add task

---
+++ src/homework/day8/ReadReplaceAndAddTask.java
@@ -0,0 +1,31 @@
+package homework.day8;
+
+import java.io.*;
+
+public class ReadReplaceAndAddTask {
+
+    public static void main(String[] args) {
+
+        String file = "file_with_phrase.txt";
+        ReadReplaceAndAddTask readReplaceAndAddTask = new ReadReplaceAndAddTask();
+        readReplaceAndAddTask.readReplaceAndAddText(file);
+    }
+
+    public void readReplaceAndAddText(String fileName) {
+        String textFromFile = "";
+        try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
+            String line = in.readLine();
+            while (line != null) {
+                textFromFile += line + "\n";
+                line = in.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass() + ": " + e.getMessage());
+        }
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true))) {
+            out.write("\n" + "\n" + textFromFile.replaceAll("[bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ]", ""));
+        } catch (IOException e) {
+            System.out.println(e.getClass() + ": " + e.getMessage());
+        }
+    }
+}
