f4c5a4fd603ae8f964ca61f232033fe64462131e
Artem Rusak <tema.rusak88@gmail.com>
2024-12-03 18:38:08 +0300

    cars task

---
+++ src/homework/day9/CarsRunner.java
@@ -0,0 +1,13 @@
+package homework.day9;
+
+import java.util.stream.Stream;
+
+public class CarsRunner {
+
+    public static void main(String[] args) {
+
+        Stream<String> cars = Stream.of("Мерс", "Ауди", "Жигуль", "Рено", "Жигуль", "Жигуль", "Ауди");
+
+        cars.distinct().filter(car -> car.contains("и")).skip(1).map(String::toUpperCase).forEach(System.out::println);
+    }
+}
