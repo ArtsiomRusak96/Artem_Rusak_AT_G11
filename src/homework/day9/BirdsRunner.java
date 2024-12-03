1a3f90f32115a89c9bfd9256b047bd4022d63d94
Artem Rusak <tema.rusak88@gmail.com>
2024-12-03 18:43:25 +0300

    birds task

---
+++ src/homework/day9/BirdsRunner.java
@@ -0,0 +1,20 @@
+package homework.day9;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BirdsRunner {
+
+    public static void main(String[] args) {
+
+        List<String> birds = Arrays.asList("Чайка", "Дрозд", "Бусел", "Голубь", "Воробей", "Цапля");
+
+        Arrays.stream(birds.stream()
+                        .map(bird -> bird.replaceAll("о", "а"))
+                        .collect(Collectors.joining()).toLowerCase()
+                        .replaceAll("ь", "")
+                        .split("б")).map(bird -> "--" + bird + "--")
+                        .forEach(System.out::println);
+    }
+}
