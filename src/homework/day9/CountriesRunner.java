1fde893a75387bee4fd0613d17b1a209ce85aea9
Artem Rusak <tema.rusak88@gmail.com>
2024-12-03 18:40:46 +0300

    countries task

---
+++ src/homework/day9/CountriesRunner.java
@@ -0,0 +1,17 @@
+package homework.day9;
+
+import java.util.stream.Stream;
+
+public class CountriesRunner {
+
+    public static void main(String[] args) {
+
+        Stream<String> countries = Stream.of("Андора", "Португалия", "Англия", "Замбия");
+
+        countries.filter(country -> country.matches(".*(?i)[аеёиоуыэюя]*."))
+                .filter(country -> country.length() < 7)
+                .map(String::toUpperCase)
+                .map(country -> "\"" + country + "\"")
+                .forEach(System.out::println);
+    }
+}
