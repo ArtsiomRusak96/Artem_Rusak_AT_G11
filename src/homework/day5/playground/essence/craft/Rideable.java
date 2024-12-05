64702d3c941c5f1282cb9df8581e5cea18869874
Artem Rusak <tema.rusak88@gmail.com>
2024-12-05 21:44:50 +0300

    created objects in RideableProcessorRunner class

--- src/homework/day5/playground/essence/craft/Rideable.java
+++ src/homework/day5/playground/essence/craft/Rideable.java
@@ -5,6 +5,6 @@ public interface Rideable {
     String getName();
 
     default void drive(String direction) {
-        System.out.printf("I am %s, my name is %s and I am driving to %s", getClass().getSimpleName(), getName(), direction);
+        System.out.printf("I am %s, my name is %s and I am driving to %s\n", getClass().getSimpleName(), getName(), direction);
     }
 }
