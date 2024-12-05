56029e78c7ff18b76d9cf6f4703999e83b21ddfc
Artem Rusak <tema.rusak88@gmail.com>
2024-12-05 22:07:37 +0300

    updated Crawlable interface

--- src/homework/day5/playground/essence/creatures/Crawlable.java
+++ src/homework/day5/playground/essence/creatures/Crawlable.java
@@ -1,4 +1,15 @@
 package homework.day5.playground.essence.creatures;
 
 public interface Crawlable {
+
+    String getName();
+
+    default void crawl(String direction, int distance) {
+        System.out.printf("I am %s, my name is %s and I am crawling to %s for %d units\n", getClass().getSimpleName(), getName(), direction, distance);
+        if (this instanceof Crocodile) {
+            System.out.println("wr-wr-wrr-r..");
+        } else if (this instanceof Beetle) {
+            System.out.println("vz-vz-vzz-zz..");
+        }
+    }
 }
